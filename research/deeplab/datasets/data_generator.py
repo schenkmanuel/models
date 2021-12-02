68a8a5c30f0e4668633bd23245b730b30c228110
schenkmanuel <88664850+schenkmanuel@users.noreply.github.com>
2021-12-02 20:23:20 +0100

    changes for outsticking_manholes

--- research/deeplab/datasets/data_generator.py
+++ research/deeplab/datasets/data_generator.py
@@ -99,10 +99,21 @@ _ADE20K_INFORMATION = DatasetDescriptor(
     ignore_label=0,
 )
 
+_OUTSTICKING_MANHOLES = DatasetDescriptor(
+    splits_to_sizes={
+        'train': 70,  # num of samples in train.txt
+        'val': 20,  # num of samples in val.txt
+        'test': 10,
+    },
+    num_classes=3, # classes+bg+ignore_label
+    ignore_label=255,
+)
+
 _DATASETS_INFORMATION = {
     'cityscapes': _CITYSCAPES_INFORMATION,
     'pascal_voc_seg': _PASCAL_VOC_SEG_INFORMATION,
     'ade20k': _ADE20K_INFORMATION,
+    'outsticking_manholes': _OUTSTICKING_MANHOLES # own dataset
 }
 
 # Default file pattern of TFRecord of TensorFlow Example.
